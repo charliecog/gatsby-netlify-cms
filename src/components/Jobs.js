6f3fcd1835f5b89c29bd7270c878c62fa780917d
charliecog <charliecoggans@hotmail.com>
2020-06-24 20:58:33 +0100

    adds jobs page with nav links

---
+++ src/components/Jobs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Jobs extends React.Component {
+    render() {
+        return (
+            <>
+                <h2>This is the jobs Component</h2>
+                <button>Click me</button>
+            </>
+        )
+    }
+}
+
+export default Jobs
